e82e4a9ddadc7ac569d7000ed7d28db21651ac28
Roman Nagibov <roman.nagibov@mail.ru>
2017-10-19 19:23:56 +0300

    fix board

--- js/main.js
+++ js/main.js
@@ -1,295 +1,201 @@
-let playerImage     = new Image();
-playerImage.src     = "images/o.png";
-let computerImage   = new Image();
-computerImage.src   = "images/x.png";
-let board           = [];
-let HALF_BOARD_SIZE = 3;
-let BOARD_SIZE      = 9;
-let WIN_COMBINATION = 3;
-let UNOCCUPIED      = ' ';
-let HUMAN_PLAYER    = 'O';
-let COMPUTER_PLAYER = 'X';
-let active_turn     = "HUMAN";
-let choice;
-let searchTimes     = [];
-let showAverageTime = true;
+const init = {
+  board:                [],
+  BOARD_SIDE:           4,
+  BOARD_SIZE:           16,
+  WIN_COMBINATION:      3,
+  UNOCCUPIED:           '',
+  HUMAN_PLAYER_VIEW:    '<img src="images/o.png" >',
+  COMPUTER_PLAYER_VIEW: '<img src="images/x.png" >',
+  HUMAN_PLAYER:         'O',
+  COMPUTER_PLAYER:      'X',
+  active_turn:          "COMPUTER",
+  choice:               '',
+  searchTimes:          [],
+  showAverageTime:      true,
+  worker:               new Worker('js/worker.js')
+};
 
-function NewGame() {
-  LoadBorder();
-  for (let i = 0; i < BOARD_SIZE; i++) {
-    board[i]               = UNOCCUPIED;
-    document.images[i].src = "images/blank.png";
+newGame = () => {
+  loadBorder();
+
+  for (let i = 0; i < init.BOARD_SIZE; i++) {
+    init.board[i] = init.UNOCCUPIED;
   }
+  init.showAverageTime = true;
+  init.active_turn     = "HUMAN";
 
-  DeleteTimes();
-  showAverageTime = true;
-  let alert       = document.getElementById("turnInfo");
-  active_turn     = "HUMAN";
-  alert.innerHTML = "Your turn!";
+  document.getElementById("turnInfo").innerHTML = "Your turn!";
+};
+
+function setStartConfiguration() {
+
+  init.board           = [];
+  init.BOARD_SIDE      = +document.getElementById('board_size').value;
+  console.log(init.BOARD_SIDE);
+  init.WIN_COMBINATION = +document.getElementById('winner_combination').value;
+  init.BOARD_SIZE      = Math.pow(document.getElementById('board_size').value, 2);
+  newGame();
 }
 
-function SetStartConfiguration(HALF_BOARD_SIZE_NEW, WIN_COMBINATION_NEW) {
-  board           = [];
-  HALF_BOARD_SIZE = HALF_BOARD_SIZE_NEW;
-  WIN_COMBINATION = WIN_COMBINATION_NEW;
-  BOARD_SIZE      = HALF_BOARD_SIZE_NEW * HALF_BOARD_SIZE_NEW;
+function loadBorder() {
+  let board   = ``;
+  let counter = 0;
 
-  LoadBorder();
-  NewGame();
-}
-
-function LoadBorder() {
-  let board = ``;
-  for (let i = 0, counter = 0; i < HALF_BOARD_SIZE; i++) {
-    for (let j = 0; j < HALF_BOARD_SIZE; j++) {
-      //language=HTML
-      board += '\r\n' + `<img src="images/blank.png"  border="2" width="75" height="71" onclick="MakeMove(${counter++})"style="position:absolute; left:${10 + j * 100}; top:${50 + i * 100}; z-index:1" />`;
+  for (let i = 0; i < init.BOARD_SIDE; i++) {
+    for (let j = 0; j < init.BOARD_SIDE; j++) {
+      board += '\r\n' + `<div class="block" onClick="makeMove(${counter++})"  ></div>`;
     }
   }
-  document.getElementById('board').innerHTML = board;
+  document.getElementById('board').style.width = init.BOARD_SIDE * 34;
+  document.getElementById('board').innerHTML   = board;
 }
 
-function MakeMove(pos) {
-  if (!GameOver(board) && board[pos] === UNOCCUPIED) {
-    board[pos]               = HUMAN_PLAYER;
-    document.images[pos].src = playerImage.src;
-    if (!GameOver(board)) {
-      let alert       = document.getElementById("turnInfo");
-      active_turn     = "COMPUTER";
-      alert.innerHTML = "Computer's turn.";
-      MakeComputerMove();
+function makeMove(pos) {
+  if (!gameOver(init.board) && init.board[pos] === init.UNOCCUPIED && init.active_turn === "HUMAN") {
+    init.board[pos]                                         = init.HUMAN_PLAYER;
+    document.getElementsByClassName('block')[pos].innerHTML = init.HUMAN_PLAYER_VIEW;
+    if (!gameOver(init.board)) {
+      init.active_turn                              = "COMPUTER";
+      document.getElementById("turnInfo").innerHTML = "Computer's turn.";
+      makeComputerMove();
     }
   }
 }
-function alphaBetaMinimax(node, depth, alpha, beta) {
 
-  if (CheckForWinner(node) === 1 || CheckForWinner(node) === 2
-    || CheckForWinner(node) === 3)
-    return GameScore(node, depth);
-
-  depth += 1;
-  let availableMoves = GetAvailableMoves(node);
-  let move, result, possible_game;
-  if (active_turn === "COMPUTER") {
-    for (let i = 0; i < availableMoves.length; i++) {
-      move          = availableMoves[i];
-      possible_game = GetNewState(move, node);
-      result        = alphaBetaMinimax(possible_game, depth, alpha, beta);
-      node          = UndoMove(node, move);
-      if (result > alpha) {
-        alpha = result;
-        if (depth === 1)
-          choice = move;
-      } else if (alpha >= beta  || depth<BOARD_SIZE) {
-        return alpha;
-      }
-    }
-    return alpha;
-  } else {
-    for (let i = 0; i < availableMoves.length; i++) {
-      move          = availableMoves[i];
-      possible_game = GetNewState(move, node);
-      result        = alphaBetaMinimax(possible_game, depth, alpha, beta);
-      node          = UndoMove(node, move);
-      if (result < beta) {
-        beta = result;
-        if (depth === 1)
-          choice = move;
-      } else if (beta <= alpha || BOARD_SIZE<16) {
-        return beta;
-      }
-    }
-
-    return beta;
-  }
-}
-function MakeComputerMove() {
+function makeComputerMove() {
   let start, end, time;
   start = new Date().getTime() / 1000;
-  if (window.Worker) {
-    let worker = new Worker("js/worker.js");
-    worker.postMessage([board, 0, -Infinity, +Infinity]);
-    worker.onmessage = function (e) {
-      end  = new Date().getTime() / 1000;
-      time = end - start;
-      ShowTimes(time);
-      let move                  = choice;
-      board[move]               = COMPUTER_PLAYER;
-      document.images[move].src = computerImage.src;
-      choice                    = [];
-      active_turn               = "HUMAN";
-      if (!GameOver(board)) {
-        let alert       = document.getElementById("turnInfo");
-        alert.innerHTML = "Your turn!";
-      }
-    };
-    // alphaBetaMinimax(board, 0, -Infinity, +Infinity);
+  init.worker.postMessage([JSON.stringify(init), 0, -Infinity, +Infinity]);
+  init.worker.addEventListener('message', function (event) {
+    if (event.data === init.choice) {
+      return;
+    }
+    init.choice = event.data;
+    end         = new Date().getTime() / 1000;
+    time        = end - start;
+    if (time < 1000) {
+      window.setTimeout(makeComputerMoveAfterDelay, 1000 - time);
+      return;
+    }
+    window.setTimeout(makeComputerMoveAfterDelay);
+  });
+}
+function makeComputerMoveAfterDelay() {
+  const move                                               = init.choice;
+  init.board[move]                                         = init.COMPUTER_PLAYER;
+  document.getElementsByClassName('block')[move].innerHTML = init.COMPUTER_PLAYER_VIEW;
+  init.choice                                              = [];
+  init.active_turn                                         = "HUMAN";
+  if (!gameOver(init.board)) {
+    const alert     = document.getElementById("turnInfo");
+    alert.innerHTML = "Your turn!";
   }
 }
 
-/**
- * @return {number}
- */
-function GameScore(game, depth) {
-  let score = CheckForWinner(game);
-  if (score === 1)
-    return 0;
-  else if (score === 2)
-    return depth - 10;
-  else if (score === 3)
-    return 10 - depth;
-}
+function checkForWinner(game) {
 
-function UndoMove(game, move) {
-  game[move] = UNOCCUPIED;
-  ChangeTurn();
-  return game;
-}
-
-function GetNewState(move, game) {
-  game[move] = ChangeTurn();
-  return game;
-}
-
-function ChangeTurn() {
-  let piece;
-  if (active_turn === "COMPUTER") {
-    piece       = 'X';
-    active_turn = "HUMAN";
-  } else {
-    piece       = 'O';
-    active_turn = "COMPUTER";
-  }
-  return piece;
-}
-
-function GetAvailableMoves(game) {
-  let possibleMoves = [];
-  for (let i = 0; i < BOARD_SIZE; i++) {
-    if (game[i] === UNOCCUPIED)
-      possibleMoves.push(i);
-  }
-  return possibleMoves;
-}
-
-// Check for a winner.  Return
-//   0 if no winner or tie yet
-//   1 if it's a tie
-//   2 if HUMAN_PLAYER won
-//   3 if COMPUTER_PLAYER won
-/**
- * @return {number}
- */
-function CheckForWinner(game) {
-  // Check for horizontal wins
-  // console.log(game);
-  for (let i = 0; i < HALF_BOARD_SIZE * HALF_BOARD_SIZE; i = i + HALF_BOARD_SIZE) {
+  for (let i = 0; i < init.BOARD_SIDE * init.BOARD_SIDE; i = i + init.BOARD_SIDE) {
     let human = 0, computer = 0;
-    for (let j = i; j <= i + HALF_BOARD_SIZE - 1; j++) {
-      if (game[j] === HUMAN_PLAYER) {
+    for (let j = i; j < i + init.BOARD_SIDE; j++) {
+      if (game[j] === init.HUMAN_PLAYER) {
         human++;
-        //console.log("j", j);
       }
       else {
         human = 0;
       }
-      if (game[j] === COMPUTER_PLAYER) {
+      if (game[j] === init.COMPUTER_PLAYER) {
         computer++;
       }
       else {
         computer = 0;
       }
-      if (computer === WIN_COMBINATION) {
+      if (computer === init.WIN_COMBINATION) {
         return 3;
       }
-      // console.log("aas", human);
-      if (human === WIN_COMBINATION) {
+      if (human === init.WIN_COMBINATION) {
         return 2;
       }
     }
   }
 
-  //Check for vertical wins
-  for (let i = 0; i < HALF_BOARD_SIZE; i++) {
+  for (let i = 0; i < init.BOARD_SIDE; i++) {
     let human = 0, computer = 0;
-    for (let j = i; j < HALF_BOARD_SIZE * HALF_BOARD_SIZE; j = j + HALF_BOARD_SIZE) {
-      if (game[j] === HUMAN_PLAYER) {
+    for (let j = i; j < init.BOARD_SIDE * init.BOARD_SIDE; j = j + init.BOARD_SIDE) {
+      if (game[j] === init.HUMAN_PLAYER) {
         human++;
       }
       else {
         human = 0;
+      }
+      if (game[j] === init.COMPUTER_PLAYER) {
+        computer++;
+      }
+      else {
+        computer = 0;
+      }
+      if (computer === init.WIN_COMBINATION) {
+        return 3;
+      }
+      if (human === init.WIN_COMBINATION) {
+        return 2;
+      }
+    }
+  }
 
-        if (game[j] === COMPUTER_PLAYER) {
-          computer++;
-        }
-        else {
-          computer = 0;
-        }
-        if (computer === WIN_COMBINATION) {
-          return 3;
-        }
-        if (human === WIN_COMBINATION) {
-          return 2;
-        }
-      }
-    }
-  }
-//Check for diagonal top-right wins
-  for (let i = HALF_BOARD_SIZE - 1; i >= 0; i--) {
+  for (let i = init.BOARD_SIDE - 1; i >= 0; i--) {
     let human = 0, computer = 0;
-    for (let j = i, counter = i; j < HALF_BOARD_SIZE * HALF_BOARD_SIZE; j = j + HALF_BOARD_SIZE, counter--) {
-      for (let k = j; counter >= 0; k = k + HALF_BOARD_SIZE - 1, counter--) {
-        if (game[k] === HUMAN_PLAYER) {
+    for (let j = i, counter = i; j < init.BOARD_SIDE * init.BOARD_SIDE; j = j + init.BOARD_SIDE, counter--) {
+      for (let k = j; counter >= 0; k = k + init.BOARD_SIDE - 1, counter--) {
+        if (game[k] === init.HUMAN_PLAYER) {
           human++;
         }
         else {
           human = 0;
         }
-        if (game[k] === COMPUTER_PLAYER) {
+        if (game[k] === init.COMPUTER_PLAYER) {
           computer++;
         }
         else {
           computer = 0;
         }
-        if (computer === WIN_COMBINATION) {
+        if (computer === init.WIN_COMBINATION) {
           return 3;
         }
-        if (human === WIN_COMBINATION) {
-          return 2;
-        }
-      }
-    }
-  }
-//Check for diagonal down-left wins
-  for (let i = 0; i < HALF_BOARD_SIZE; i++) {
-    let human = 0, computer = 0;
-    for (let j = i, counter = i; j < HALF_BOARD_SIZE * HALF_BOARD_SIZE; j = j + HALF_BOARD_SIZE, counter++) {
-      for (let k = j; counter <= HALF_BOARD_SIZE; k = k + HALF_BOARD_SIZE + 1, counter++) {
-        if (game[k] === HUMAN_PLAYER) {
-          human++;
-        }
-        else {
-          human = 0;
-        }
-        if (game[k] === COMPUTER_PLAYER) {
-          computer++;
-        }
-        else {
-          computer = 0;
-        }
-        if (computer === WIN_COMBINATION) {
-          return 3;
-        }
-        if (human === WIN_COMBINATION) {
+        if (human === init.WIN_COMBINATION) {
           return 2;
         }
       }
     }
   }
 
-  // Check for tie
-  for (let i = 0; i < BOARD_SIZE; i++) {
-    if (game[i] !== HUMAN_PLAYER && game[i] !== COMPUTER_PLAYER)
+  for (let i = 0; i < init.BOARD_SIDE; i++) {
+    let human = 0, computer = 0;
+    for (let j = i, counter = i; j < init.BOARD_SIDE * init.BOARD_SIDE; j = j + init.BOARD_SIDE, counter++) {
+      for (let k = j; counter <= init.BOARD_SIDE; k = k + init.BOARD_SIDE + 1, counter++) {
+        if (game[k] === init.HUMAN_PLAYER) {
+          human++;
+        }
+        else {
+          human = 0;
+        }
+        if (game[k] === init.COMPUTER_PLAYER) {
+          computer++;
+        }
+        else {
+          computer = 0;
+        }
+        if (computer === init.WIN_COMBINATION) {
+          return 3;
+        }
+        if (human === init.WIN_COMBINATION) {
+          return 2;
+        }
+      }
+    }
+  }
+
+  for (let i = 0; i < init.BOARD_SIZE; i++) {
+    if (game[i] !== init.HUMAN_PLAYER && game[i] !== init.COMPUTER_PLAYER)
       return 0;
   }
 
@@ -297,18 +203,15 @@ function CheckForWinner(game) {
 
 }
 
-/**
- * @return {boolean}
- */
-function GameOver(game) {
-  if (CheckForWinner(game) === 0) {
+function gameOver(game) {
+  if (checkForWinner(game) === 0) {
     return false;
   }
-  else if (CheckForWinner(game) === 1) {
+  else if (checkForWinner(game) === 1) {
     let alert       = document.getElementById("turnInfo");
     alert.innerHTML = "It is a tie.";
   }
-  else if (CheckForWinner(game) === 2) {
+  else if (checkForWinner(game) === 2) {
     let alert       = document.getElementById("turnInfo");
     alert.innerHTML = "You have won! Congratulations!";
   }
@@ -316,32 +219,5 @@ function GameOver(game) {
     let alert       = document.getElementById("turnInfo");
     alert.innerHTML = "The computer has won.";
   }
-  ShowAverageTime();
   return true;
 }
-
-function ShowTimes(time) {
-  searchTimes.push(time);
-  document.getElementById("searchTime").innerHTML =
-    document.getElementById("searchTime").innerHTML + time + " seconds. <br />";
-}
-
-function DeleteTimes() {
-  searchTimes                                     = [];
-  document.getElementById("searchTime").innerHTML = "";
-}
-
-function ShowAverageTime() {
-  if (showAverageTime) {
-    let sum = 0;
-    let i   = 0;
-    for (i; i < searchTimes.length; i++)
-      sum += searchTimes[i];
-
-    document.getElementById("searchTime").innerHTML =
-      document.getElementById("searchTime").innerHTML + "<br />Average search was <strong>" + sum / i + "</strong> seconds. <br />";
-    showAverageTime                                 = false;
-  }
-}
-
-export { alphaBetaMinimax };
--- js/worker.js
+++ js/worker.js
@@ -1,11 +1,225 @@
-/**
- * Created by roman on 12.10.17.
- */
-import { alphaBetaMinimax } from 'main.js';
+self.addEventListener('message', function (event) {
+  this.inits = JSON.parse(event.data[0]);
 
-onmessage = function(e) {
-  console.log('Message received from main script');
-  let workerResult = alphaBetaMinimax(e.data[0],e.data[1],e.data[2],e.data[3]);
-  console.log('Posting message back to main script');
-  postMessage(workerResult);
-};
+  alphaBetaMinimax(this.inits.board, event.data[1], event.data[2], event.data[3]);
+  self.postMessage(this.inits.choice);
+
+  function gameScore(game, depth) {
+    let score = checkForWinner(game);
+    if (score === 1)
+      return 0;
+    else if (score === 2)
+      return depth - 10;
+    else if (score === 3)
+      return 10 - depth;
+  }
+
+  function undoMove(game, move) {
+    game[move] = this.inits.UNOCCUPIED;
+    changeTurn();
+    return game;
+  }
+
+  function getNewState(move, game) {
+    game[move] = changeTurn();
+    return game;
+  }
+
+  function changeTurn() {
+    let piece;
+    if (this.inits.active_turn === "COMPUTER") {
+      piece             = 'X';
+      this.inits.active_turn = "HUMAN";
+    } else {
+      piece             = 'O';
+      this.inits.active_turn = "COMPUTER";
+    }
+    return piece;
+  }
+
+  function getAvailableMoves(game) {
+    const possibleMoves = [];
+    for (let i = 0; i < this.inits.BOARD_SIZE; i++) {
+      if (game[i] === this.inits.UNOCCUPIED)
+        possibleMoves.push(i);
+    }
+    return possibleMoves;
+  }
+
+  function alphaBetaMinimax(node, depth, alpha, beta) {
+    if (checkForWinner(node) === 1 || checkForWinner(node) === 2
+      || checkForWinner(node) === 3) {
+      return gameScore(node, depth);
+    }
+
+    depth += 1;
+    let availableMoves = getAvailableMoves(node);
+    let move, result, possible_game;
+    if (this.inits.active_turn === "COMPUTER") {
+      for (let i = 0; i < availableMoves.length; i++) {
+        move          = availableMoves[i];
+        possible_game = getNewState(move, node);
+        result        = alphaBetaMinimax(possible_game, depth, alpha, beta);
+        node          = undoMove(node, move);
+        if (result > alpha) {
+          alpha = result;
+          if (depth === 1) {
+            this.inits.choice = move;
+          }
+        } else if (alpha >= beta || depth > 2) {
+          return alpha;
+        }
+      }
+      return alpha;
+    } else {
+      for (let i = 0; i < availableMoves.length; i++) {
+        move          = availableMoves[i];
+        possible_game = getNewState(move, node);
+        result        = alphaBetaMinimax(possible_game, depth, alpha, beta);
+        node          = undoMove(node, move);
+
+        if (result < beta) {
+          beta = result;
+          if (depth === 1) {
+            this.inits.choice = move;
+          }
+        } else if (beta <= alpha || depth > 2) {
+          return beta;
+        }
+      }
+
+      return beta;
+    }
+  }
+
+  /**
+   * Check for a winner.  Return
+   0 if no winner or tie yet
+   1 if it's a tie
+   2 if HUMAN_PLAYER won
+   3 if COMPUTER_PLAYER won
+   */
+  function checkForWinner(game) {
+    /**
+     * Check for horizontal wins
+     */
+    for (let i = 0; i < this.inits.BOARD_SIDE * this.inits.BOARD_SIDE; i = i + this.inits.BOARD_SIDE) {
+      let human = 0, computer = 0;
+      for (let j = i; j < i + this.inits.BOARD_SIDE; j++) {
+        if (game[j] === this.inits.HUMAN_PLAYER) {
+          human++;
+        }
+        else {
+          human = 0;
+        }
+        if (game[j] === this.inits.COMPUTER_PLAYER) {
+          computer++;
+        }
+        else {
+          computer = 0;
+        }
+        if (computer === this.inits.WIN_COMBINATION) {
+          return 3;
+        }
+        if (human === this.inits.WIN_COMBINATION) {
+          return 2;
+        }
+      }
+    }
+
+    /**
+     *  Check for vertical wins
+     */
+    for (let i = 0; i < this.inits.BOARD_SIDE; i++) {
+      let human = 0, computer = 0;
+      for (let j = i; j < this.inits.BOARD_SIDE * this.inits.BOARD_SIDE; j = j + this.inits.BOARD_SIDE) {
+        if (game[j] === this.inits.HUMAN_PLAYER) {
+          human++;
+        }
+        else {
+          human = 0;
+        }
+        if (game[j] === this.inits.COMPUTER_PLAYER) {
+          computer++;
+        }
+        else {
+          computer = 0;
+        }
+        if (computer === this.inits.WIN_COMBINATION) {
+          return 3;
+        }
+        if (human === this.inits.WIN_COMBINATION) {
+          return 2;
+        }
+      }
+    }
+
+    /**
+     * Check for diagonal top-right wins
+     */
+    for (let i = this.inits.BOARD_SIDE - 1; i >= 0; i--) {
+      let human = 0, computer = 0;
+      for (let j = i, counter = i; j < this.inits.BOARD_SIDE * this.inits.BOARD_SIDE; j = j + this.inits.BOARD_SIDE, counter--) {
+        for (let k = j; counter >= 0; k = k + inits.BOARD_SIDE - 1, counter--) {
+          if (game[k] === this.inits.HUMAN_PLAYER) {
+            human++;
+          }
+          else {
+            human = 0;
+          }
+          if (game[k] === this.inits.COMPUTER_PLAYER) {
+            computer++;
+          }
+          else {
+            computer = 0;
+          }
+          if (computer === this.inits.WIN_COMBINATION) {
+            return 3;
+          }
+          if (human === this.inits.WIN_COMBINATION) {
+            return 2;
+          }
+        }
+      }
+    }
+
+    /**
+     *  Check for diagonal down-left wins
+     */
+    for (let i = 0; i < this.inits.BOARD_SIDE; i++) {
+      let human = 0, computer = 0;
+      for (let j = i, counter = i; j < this.inits.BOARD_SIDE * this.inits.BOARD_SIDE; j = j + this.inits.BOARD_SIDE, counter++) {
+        for (let k = j; counter <= this.inits.BOARD_SIDE; k = k + this.inits.BOARD_SIDE + 1, counter++) {
+          if (game[k] === this.inits.HUMAN_PLAYER) {
+            human++;
+          }
+          else {
+            human = 0;
+          }
+          if (game[k] === this.inits.COMPUTER_PLAYER) {
+            computer++;
+          }
+          else {
+            computer = 0;
+          }
+          if (computer === this.inits.WIN_COMBINATION) {
+            return 3;
+          }
+          if (human === this.inits.WIN_COMBINATION) {
+            return 2;
+          }
+        }
+      }
+    }
+
+    /**
+     * Check for tie
+     */
+    for (let i = 0; i < this.inits.BOARD_SIZE; i++) {
+      if (game[i] !== this.inits.HUMAN_PLAYER && game[i] !== this.inits.COMPUTER_PLAYER)
+        return 0;
+    }
+    return 1;
+  }
+
+});
